02a87f3d64224778dc5f828fef29e0ee76bc3c23
jose zambrano <joseyzambranovpe@gmail.com>
2021-09-18 18:46:11 -0500

    frontend culminado

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,39 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Item } from '../modelos/items';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
   
-  items:Item[] =[];
+  items:Item[] = [{
+    id:0,
+    title: "manzana",
+    price: 10.5,
+    quantity:4,
+    completed:false
+  },
+  {
+    id:1,
+    title: "pera",
+    price: 3.5,
+    quantity:4,
+    completed:true
+  },
+  {
+    id:2,
+    title: "cambur",
+    price: 8.5,
+    quantity:5,
+    completed:true
+  }];
 
-  constructor(private itemService:ItemService) { }
+  constructor() { }
 
   getItem(){
-    this.items= [{
-      id:0,
-      title: "manzana",
-      price: 10.5,
-      quantity:4,
-      completed:false
-    },
-    {
-      id:1,
-      title: "pera",
-      price: 3.5,
-      quantity:4,
-      completed:true
-    },
-    {
-      id:2,
-      title: "cambur",
-      price: 8.5,
-      quantity:5,
-      completed:true
-    }];
-
     return this.items
+    
   }
   addItem(item:Item){
     this.items.unshift(item);
